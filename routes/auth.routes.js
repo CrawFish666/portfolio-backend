24fa6c99287550b312f04bc099726c6a2121b2e7
CrawFish666 <emelyakoro4@mail.ru>
2026-09-21 23:12:08 +0400

    feat: добавить маршруты с аутентификацией

---
+++ routes/auth.routes.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const router = express.Router();
+const {
+	register,
+	login,
+	refresh,
+	logout,
+	getMe,
+	updateProfile,
+	sendRecoveryPasswordToken,
+	verifyResetPwdToken,
+	resetPasswordViaToken
+} = require("../controllers/auth.controller");
+const { protect } = require("../middlewares/authMiddleware");
+const { loginLimiter, forgotPasswordLimiter } = require("../middlewares/authLimiter.middleware");
+
+router.post("/register", loginLimiter, register);
+router.post("/login", loginLimiter, login);
+router.post("/refresh", refresh);
+router.post("/logout", logout);
+router.get("/me", protect, getMe);
+router.put("/me", protect, updateProfile);
+router.post("/forgot-password", forgotPasswordLimiter, sendRecoveryPasswordToken);
+router.get("/reset-password/:token/verify", verifyResetPwdToken);
+router.post("/reset-password", forgotPasswordLimiter, resetPasswordViaToken);
+
+module.exports = router;
