348cbc28d82354e5bb84a509f5a5f07566a5e7de
CrawFish666 <emelyakoro4@mail.ru>
2026-09-22 06:08:09 +0400

    security: улучшить IP rate limiting

--- middlewares/authLimiter.middleware.js
+++ middlewares/authLimiter.middleware.js
@@ -1,9 +1,12 @@
 const rateLimit = require("express-rate-limit");
+const { ipKeyGenerator } = require("express-rate-limit");
+const getClientIp = require("../utils/getClientIp");
 
 // Защита от подбора пароля / спама регистраций.
 const loginLimiter = rateLimit({
 	windowMs: 15 * 60 * 1000, // 15 минут
 	max: 10,
+	keyGenerator: (req) => ipKeyGenerator(getClientIp(req)),
 	standardHeaders: true,
 	legacyHeaders: false,
 	skipSuccessfulRequests: true, // успешные логины не расходуют лимит
@@ -15,6 +18,7 @@ const loginLimiter = rateLimit({
 const forgotPasswordLimiter = rateLimit({
 	windowMs: 15 * 60 * 1000,
 	max: 5,
+	keyGenerator: (req) => ipKeyGenerator(getClientIp(req)),
 	standardHeaders: true,
 	legacyHeaders: false,
 	message: { message: "Слишком много запросов. Повторите позже." },
--- middlewares/feedbackLimiter.middleware.js
+++ middlewares/feedbackLimiter.middleware.js
@@ -1,8 +1,11 @@
 const rateLimit = require("express-rate-limit");
+const { ipKeyGenerator } = require("express-rate-limit");
+const getClientIp = require("../utils/getClientIp");
 
 const feedbackLimiter = rateLimit({
 	windowMs: 60 * 1000, // 1 минута 
 	max: 3,
+	keyGenerator: (req) => ipKeyGenerator(getClientIp(req)),
 	standardHeaders: true,
 	legacyHeaders: false,
 	message: {
